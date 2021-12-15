3619149775ad4aa9787cb99c91210d8d54c12db4
ChangeWithCode <fa17-bcs-199@cuiwah.edu.pk>
2021-12-15 17:51:50 +0500

    hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { NavbarComponent } from './components/navbar/navbar.component'
+import { MainBannerComponent } from './components/main-banner/main-banner.component'
+import { OurWorkComponent } from './components/our-work/our-work.component'
+import { ServicesComponent } from './components/services/services.component'
+import { WhatWeDoComponent } from './components/what-we-do/what-we-do.component'
+import { PortfolioComponent } from './components/portfolio/portfolio.component'
+import { ReviewsComponent } from './components/reviews/reviews.component'
+import { OurTeamComponent } from './components/our-team/our-team.component'
+import { ContactusComponent } from './components/contact-us/contactus.component';
+import { FooterComponent } from './components/footer/footer.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainBannerComponent,
+    OurWorkComponent,
+    ServicesComponent,
+    WhatWeDoComponent,
+    PortfolioComponent,
+    ReviewsComponent,
+    OurTeamComponent,
+    ContactusComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
